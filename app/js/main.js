77bed9defac332364c2ee70646b73414ba4d70af
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-06-14 15:00:21 +0700

    fix margin in faq with js

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,26 @@
+//кнопка открыть вопрос в блоке faq
 const question_button = document.querySelectorAll('.question__button');
 
+function calculateQuestionMargin(question){
+    const height = question.lastElementChild.scrollHeight;
+    if(question.classList.contains('question_open')){
+        question.style.marginBottom = "0px";
+    }
+    else{
+        question.style.marginBottom = "-" + height + "px";
+    }
+}
+
 question_button.forEach((item) => {
+
+    const question = item.parentElement.parentElement;
+    calculateQuestionMargin(question);
     item.addEventListener('click', (e) => {
-        item.parentElement.parentElement.classList.toggle('question_open');
+        
+        question.classList.toggle('question_open');
+
+        //анимация margin-bottom у question
+        calculateQuestionMargin(question);
     })
 });
 
