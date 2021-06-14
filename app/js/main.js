3104870e9f65b296125ada7b8f8da54041430129
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-06-14 15:31:45 +0700

    fix start calculate margin bug

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,30 @@
-//кнопка открыть вопрос в блоке faq
-const question_button = document.querySelectorAll('.question__button');
+window.onload = function() {
 
-function calculateQuestionMargin(question){
-    const height = question.lastElementChild.scrollHeight;
-    if(question.classList.contains('question_open')){
-        question.style.marginBottom = "0px";
+    //кнопка открыть вопрос в блоке faq
+    const question_button = document.querySelectorAll('.question__button');
+
+    function calculateQuestionMargin(question){
+        const height = question.lastElementChild.scrollHeight - 10;
+        if(question.classList.contains('question_open')){
+            question.style.marginBottom = "0px";
+        }
+        else{
+            question.style.marginBottom = "-" + height + "px";
+        }
     }
-    else{
-        question.style.marginBottom = "-" + height + "px";
-    }
+
+    question_button.forEach((item) => {
+        const question = item.parentElement.parentElement;
+        calculateQuestionMargin(question);
+        item.addEventListener('click', (e) => {
+            
+            question.classList.toggle('question_open');
+
+            //анимация margin-bottom у question
+            calculateQuestionMargin(question);
+        })
+    });
+
 }
 
-question_button.forEach((item) => {
-
-    const question = item.parentElement.parentElement;
-    calculateQuestionMargin(question);
-    item.addEventListener('click', (e) => {
-        
-        question.classList.toggle('question_open');
-
-        //анимация margin-bottom у question
-        calculateQuestionMargin(question);
-    })
-});
 
