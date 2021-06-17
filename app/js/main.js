8a49a5efac51aade8bd03807430034ed5e183c2e
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-06-17 13:19:27 +0700

    remove menu if click link (fix faq animation)

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ window.onload = function() {
     //кнопка открыть вопрос в блоке faq
     const question_button = document.querySelectorAll('.question__button');
 
-    function calculateQuestionMargin(question){
+    /*function calculateQuestionMargin(question){
         const height = question.lastElementChild.scrollHeight - 10;
         if(question.classList.contains('question_open')){
             question.style.marginBottom = "0px";
@@ -11,17 +11,17 @@ window.onload = function() {
         else{
             question.style.marginBottom = "-" + height + "px";
         }
-    }
+    }*/
 
     question_button.forEach((item) => {
         const question = item.parentElement.parentElement;
-        calculateQuestionMargin(question);
+        //calculateQuestionMargin(question);
         item.addEventListener('click', (e) => {
             
             question.classList.toggle('question_open');
 
             //анимация margin-bottom у question
-            calculateQuestionMargin(question);
+            //calculateQuestionMargin(question);
         })
     });
 
@@ -36,21 +36,20 @@ window.onload = function() {
     })
 
     //нажатие на бургер меню
-    const burger_button = document.querySelectorAll('.burger-button');
+    const burger_button = document.querySelector('.burger-button');
+    const nav = burger_button.nextElementSibling;
+    burger_button.addEventListener('click', toggleBurgerMenu);
 
-    burger_button.forEach((item) => {
-        const nav = item.nextElementSibling;
-        item.addEventListener('click', () => {
-            item.classList.toggle('burger-button_active');
-            nav.classList.toggle('nav_active');
-            if(item.classList.contains('burger-button_active')){
-                document.body.style.position = 'fixed';
-            }
-            else{
-                document.body.style.position = '';
-            }
-        })
-    })
+    function toggleBurgerMenu(){
+        burger_button.classList.toggle('burger-button_active');
+        nav.classList.toggle('nav_active');
+        if(burger_button.classList.contains('burger-button_active')){
+            document.body.style.position = 'fixed';
+        }
+        else{
+            document.body.style.position = '';
+        }
+    }
 
     //анимация ссылок-якорей
     const links = document.querySelectorAll('.nav__link');
@@ -59,11 +58,12 @@ window.onload = function() {
 
         link.addEventListener('click', (e) => {
             e.preventDefault();
-
+            toggleBurgerMenu();
             document.getElementById(blockId).scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             })
+            
         })
     })
 
