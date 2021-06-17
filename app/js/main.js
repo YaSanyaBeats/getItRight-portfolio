d5ab0e780df8b1e79c1cff6d2d2669aaca641143
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2021-06-17 16:39:23 +0700

    git anchor links on desktop

--- app/js/main.js
+++ app/js/main.js
@@ -3,25 +3,11 @@ window.onload = function() {
     //кнопка открыть вопрос в блоке faq
     const question_button = document.querySelectorAll('.question__button');
 
-    /*function calculateQuestionMargin(question){
-        const height = question.lastElementChild.scrollHeight - 10;
-        if(question.classList.contains('question_open')){
-            question.style.marginBottom = "0px";
-        }
-        else{
-            question.style.marginBottom = "-" + height + "px";
-        }
-    }*/
 
     question_button.forEach((item) => {
         const question = item.parentElement.parentElement;
-        //calculateQuestionMargin(question);
         item.addEventListener('click', (e) => {
-            
             question.classList.toggle('question_open');
-
-            //анимация margin-bottom у question
-            //calculateQuestionMargin(question);
         })
     });
 
@@ -58,7 +44,9 @@ window.onload = function() {
 
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            toggleBurgerMenu();
+            burger_button.classList.remove('burger-button_active');
+            nav.classList.remove('nav_active');
+            document.body.style.position = '';
             document.getElementById(blockId).scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
